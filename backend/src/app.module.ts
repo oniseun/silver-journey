7411b606b912172591860199fbb794e124e61547
oniseun <seunoni34@gmail.com>
2024-06-08 21:02:48 +0100

    chore: use nest config for environment variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
-import { Questionnaire } from './questionnaire/questionnaire.entity';
+import { Questionnaire } from './questionnaire/entities/questionnaire.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'healthcare',
-      entities: [Questionnaire],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Questionnaire],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     QuestionnaireModule,
   ],
